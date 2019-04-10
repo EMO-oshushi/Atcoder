919b65185693ef5f81d866240cded58ec4e517cc
EMO-oshushi <tamai-tatsuya-hm@ynu.jp>
2019-04-10 20:48:49 +0900

    解けてなかったABC122Dを説解いた

--- ABC/ABC12x/ABC122D.py
+++ ABC/ABC12x/ABC122D.py
@@ -1,8 +1,35 @@
+MOD = 10**9+7
+
 N = int(input())
 
-ans = [0, 1, 4, 16, 63, ]
-for i in range(N+1):
-    if i <= 4:
-        continue
-    ans.append(ans[i-1] * 4 - ans[i-3] - 3 * ans[i-4])
-print(ans[N])
+countA = 4
+countG = 4
+countAG = 1
+countGA = 1
+countAC = 1
+countAGX = 0
+countAXG = 0
+countAT = 1
+ans = 16
+nextnums = [0 for _ in range(7)]
+for i in range(N-2):
+    nextans = ans*4 - countAG - countGA - countAC - countAXG - countAGX
+    nextA = ans
+    nextG = ans - countAC
+    nextAG = countA
+    nextGA = countG
+    nextAC = countA - countGA
+    nextAGX = 2 * countAG
+    nextAXG = countAT
+    nextAT = countA
+
+    ans = nextans % MOD
+    countA = nextA % MOD
+    countAC = nextAC % MOD
+    countAG = nextAG % MOD
+    countAGX = nextAGX % MOD
+    countAT = nextAT % MOD
+    countAXG = nextAXG % MOD
+    countG = nextG % MOD
+    countGA = nextGA % MOD
+print(ans)
